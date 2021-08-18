ed3e0d0a891cb154498b1d2ab0c6f88c1ce7a1ea
SteeleTyler <tylermsteele@outlook.com>
2021-08-17 21:25:07 -0400

    Create a simple flame game

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,23 @@
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
+import 'game_engine.dart';
+
 void main() {
-  runApp(MyApp());
+  runApp(GameWidget(
+    game: GameEngine(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Dungeon Crawler',
-      theme: ThemeData(
-        primarySwatch: Colors.brown,
-      ),
-      home: MyHomePage(title: 'Home Page'),
-    );
+        title: 'Dungeon Crawler',
+        theme: ThemeData(
+          primarySwatch: Colors.brown,
+        ),
+        home: MyHomePage(title: 'Home Page'));
   }
 }
 
@@ -34,9 +38,11 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-        ),
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          GameWidget(
+            game: GameEngine(),
+          )
+        ]),
       ),
     );
   }
